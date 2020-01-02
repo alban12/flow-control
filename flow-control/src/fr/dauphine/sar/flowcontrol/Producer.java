fa47fcbaa939254c7dc973353c1dd3bde2ebf9f4
Alban Tiacoh <alban.kdf@gmail.com>
2020-01-02 06:56:05 +0100

    Add project structure

---
+++ flow-control/src/fr/dauphine/sar/flowcontrol/Producer.java
@@ -0,0 +1,8 @@
+package fr.dauphine.sar.flowcontrol;
+
+import java.io.*;
+import java.net.*;
+
+public class Producer {
+
+}
---
+++ flow-control/src/fr/dauphine/sar/flowcontrol/TServer.java
@@ -0,0 +1,14 @@
+package fr.dauphine.sar.flowcontrol;
+
+import java.io.*;
+import java.net.*;
+
+public class TServer {
+	private Object[] tampon = new Object[N];
+	private static final int N=0;
+	
+	public TServer()
+	{
+		
+	}
+}
