8d27b109426364f61560a4f5c859a64f5673aa66
bahth18 <thierno.bah@dauphine.eu>
2020-01-03 10:47:09 +0100

    Creation of the server skeleton.

--- flow-control/src/fr/dauphine/sar/flowcontrol/TServer.java
+++ flow-control/src/fr/dauphine/sar/flowcontrol/TServer.java
@@ -3,12 +3,29 @@ package fr.dauphine.sar.flowcontrol;
 import java.io.*;
 import java.net.*;
 
-public class TServer {
+public class TServer  extends Thread{
+	
 	private Object[] tampon = new Object[N];
 	private static final int N=0;
+	public static final int port = 1234;
 	
 	public TServer()
 	{
 		
+	}
+	
+	@Override
+	public void run() {
+		try {
+			ServerSocket ss = new ServerSocket(port);
+			System.out.println("Server start");
+			
+			while(true) {
+				Socket s = ss.accept();
+				new Client(s).start();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
